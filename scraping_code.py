500da4db58f91c249d36cce20a5b6aa61df13495
Charlie Willmore <ccwillmore@gmail.com>
2021-06-24 10:53:18 -0500

    completed revision of scraping code and created app.py

--- scraping_code.py
+++ scraping_code.py
@@ -7,44 +7,50 @@ import pandas as pd
 
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
+# First scrape for article title
 
-url = 'https://redplanetscience.com'
-browser.visit(url)
-# Optional delay for loading page
-browser.is_element_present_by_css('div.list_text',wait_time = 1)
+def mars_news(browser):
+    url = 'https://redplanetscience.com'
+    browser.visit(url)
+    # Optional delay for loading page
+    browser.is_element_present_by_css('div.list_text',wait_time = 1)
+    html = browser.html
+    try:
+        news_soup = bs(html, 'html.parser')
+        slide_elem = news_soup.select_one('div.list_text')
+        article = slide_elem.find('div',class_='content_title').get_text()
+        summary = slide_elem.find('div', class_='article_teaser_body').get_text()
+    except AttributeError:
+        return None, None
+return article, summary
 
-html = browser.html
-news_soup = bs(html, 'html.parser')
-slide_elem = news_soup.select_one('div.list_text')
-print(slide_elem)
+def featured_image(browser):
+    url = 'https://spaceimages-mars.com'
+    browser.visit(url)
+# find and click the full image button
+    full_image_elem = browser.find_by_tag('button')[1]
+    full_image_elem.click()
+# scrape to get the 
+    html = browser.html
+    image_soup = bs(html,'html.parser')
+    try:
+        image_url_rel = image_soup.find('img',class_='fancybox-image').get('src')
+    except AttributeError:
+        return None
+    img_url = f'https://spaceimages-mars.com/{image_url_rel}'
 
+    return img_url
 
-article = slide_elem.find('div',class_='content_title').get_text()
-summary = slide_elem.find('div', class_='article_teaser_body').get_text()
-print(article)
-print(summary)
+def mars_facts(browser):
+    try:
+        mars_facts_df = pd.read_html('https://galaxyfacts-mars.com')[0]
+    except BaseException:
+        return None
 
+    mars_facts_df.columns=['description','Mars','Earth']
+    mars_facts_df.set_index('description',inplace=True)
+    mars_facts_df.head()
 
-url = 'https://spaceimages-mars.com'
-browser.visit(url)
-
-full_image_elem = browser.find_by_tag('button')[1]
-full_image_elem.click()
-
-html = browser.html
-image_soup = bs(html,'html.parser')
-
-image_url_rel = image_soup.find('img',class_='fancybox-image').get('src')
-
-final_image_url = f'https://spaceimages-mars.com/{image_url_rel}'
-print(final_image_url)
-
-mars_facts_df = pd.read_html('https://galaxyfacts-mars.com')[0]
-mars_facts_df.columns=['description','Mars','Earth']
-mars_facts_df.set_index('description',inplace=True)
-mars_facts_df.head()
-
-mars_facts_df.to_html()
-
+    return mars_facts_df.to_html()
 
 browser.quit()
